a16e1b857472ba4790f3a93386211649b93d2e0c
Diego Alejandro Murcia Cespedes <diegomurciac2220@gmail.com>
2021-01-27 09:21:59 -0500

    Part I finished

---
+++ ANSWER.md
@@ -0,0 +1,13 @@
+# LABORATORIO No.02 - ARSW
+
+```
+Integrantes:
+    Diego Alejandro Murcia Cespedes
+```
+
+## PARTE I
+1. Al ejecutar el programa y abrir el administrador de procesos se puede ver que para la ejecución de
+    este programa utiliza todos los núcleos:
+   
+    ![](/img/Monitor_Proceso.png)
+
--- parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -1,14 +1,27 @@
 package edu.eci.arsw.primefinder;
 
-public class Main {
+import java.util.LinkedList;
 
+public class Main {
 	public static void main(String[] args) {
-		
-		PrimeFinderThread pft=new PrimeFinderThread(0, 30000000);
-		
-		pft.start();
-		
-		
+		int a,b,range,threadNumber,firstNumber,lastNumber;
+		LinkedList<PrimeFinderThread> primeFinderThreads = new LinkedList<>();
+
+		a = 0;
+		b = 30000000;
+		threadNumber = 3;
+		range = (b-a)/threadNumber;
+
+		for(int i=0;i<threadNumber;i++){
+			firstNumber = range*i;
+			lastNumber = range*(i+1);
+
+			primeFinderThreads.add(new PrimeFinderThread(firstNumber,lastNumber));
+		}
+
+		for(PrimeFinderThread thread:primeFinderThreads){
+			thread.start();
+		}
 	}
 	
 }
